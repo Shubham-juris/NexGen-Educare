c19c2179cfd3197936ec19641109059db0a62940
Shubham Kumar <sk6930037@gmail.com>
2025-01-10 22:08:28 +0530

    contact us ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import HeroSection from './Components/Hero/Hero'
-import MainCards from './Components/Courses/MainCards'
-
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import HeroSection from './Components/Hero/Hero';
+import MainCards from './Components/Courses/MainCards';
+import ContactUs from './Components/Contact-Us/Contact';
 
 function App() {
-
   return (
     <>
       <Navbar />
       <HeroSection />
       <MainCards />
+      <ContactUs />
     </>
   );
 }
 
-export default App
+export default App;
