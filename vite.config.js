520d9b8f3f466fbaf29db1b50c456fe9d63004c2
Shubham Kumar <sk6930037@gmail.com>
2025-01-16 22:24:24 +0530

    lo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     chunkSizeWarningLimit: 6000,
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import path from 'path';
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 6000, 
+    chunkSizeWarningLimit: 6000,
   },
 });
