88243df6983c08c7b442672c249a52b5974cb653
Shubham Kumar <sk6930037@gmail.com>
2025-01-16 22:03:09 +0530

    build code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 6000, // Set limit to 1MB
+    chunkSizeWarningLimit: 6000, 
   },
 });
